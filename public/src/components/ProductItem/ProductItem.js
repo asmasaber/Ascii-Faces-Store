ca6efebf2fdceda219f02fe9563a539217be85cf
Asma <asmaasaber@Asmaas-MacBook-Pro.local>
2022-04-15 06:59:31 +0200

    product item & list (UI)

--- public/src/components/ProductItem/ProductItem.js
+++ public/src/components/ProductItem/ProductItem.js
@@ -1,6 +1,54 @@
 /* 
   #TODO: Product Item Component
-    - [ ] Card
-    - [ ] Format relative time 
-    - [ ] Format Price
+    - [x] Card
+    - [x] Format relative time 
+    - [x] Format Price
 */
+
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+import './ProductItem.css';
+
+const ProductItem = ({ product }) => {
+  const time = useMemo(() => {
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerWeek = msPerDay * 7;
+
+    var elapsed =  +new Date() - +new Date(product.date)
+    if (elapsed < msPerMinute) {
+      return Math.round(elapsed / 1000) + ' seconds';
+    } else if (elapsed < msPerHour) {
+      return Math.round(elapsed / msPerMinute) + ' minutes';
+    } else if (elapsed < msPerDay) {
+      return Math.round(elapsed / msPerHour) + ' hours';
+    } else if (elapsed < msPerWeek) {
+      return Math.round(elapsed / msPerDay) + ' day';
+    } else {
+      return new Date(product.date).toLocaleDateString();
+    }
+  });
+
+  return (
+    <div className="card">
+      <div className="badge">${product.price.toFixed(2)}</div>
+      <div className="content" style={{ fontSize: product.size }}>{product.face}</div>
+      <div className="details">
+        <span>since:</span> {time}
+      </div>
+    </div>
+  );
+}
+
+ProductItem.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string,
+    size: PropTypes.number,
+    price: PropTypes.number,
+    date: PropTypes.date
+  }).isRequired,
+};
+
+export { ProductItem };
--- public/src/components/ProductList/ProductList.js
+++ public/src/components/ProductList/ProductList.js
@@ -7,17 +7,40 @@
     - [ ] End of Products Text
 */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Loading } from '../Loading/Loading';
 import { SortForm } from '../SortForm/SortForm';
+import { ProductItem } from '../ProductItem/ProductItem';
+
 import './ProductList.css';
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchProducts = async() => {
+    setLoading(true);
+    const response = await fetch('http://localhost:3000/api/products');
+    const products = await response.json();
+    setLoading(false);
+    setProducts(products);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <Loading />
+      {loading && <Loading />}
       <SortForm />
-      <h1>Product List</h1>
+      <div className="row">
+        {products.map((p) => 
+          <div className="column" key={p.id}>
+            <ProductItem product={p}/>
+          </div>
+        )}
+      </div>
     </>
   );
 }
